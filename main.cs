6521d5947593f36b277c760c1a47aa59398390f4
Catten <99224186+CattenWithGun@users.noreply.github.com>
2024-02-27 12:58:19 -0800

    Updated NeuralNetwork class, with 2 constructors

--- main.cs
+++ main.cs
@@ -5,40 +5,81 @@ public class Program
   public static void Main()
   {
     NeuralNetwork network = new NeuralNetwork();
-    Console.WriteLine("Hello World");
+    Console.WriteLine("quell");
   }
 }
 
 public class NeuralNetwork
 {
   //Layers
-  byte[] inputLayer = new byte[784];
-  byte[] hiddenLayer1 = new byte[16];
-  byte[] hiddenLayer2 = new byte[16];
-  byte[] outputLayer = new byte[10];
+  public double[] inputLayer;
+  public double[] hiddenLayer1;
+  public double[] hiddenLayer2;
+  public double[] outputLayer;
 
   //Weights
-  byte[,] inputLayerWeights = RandomStartingWeights(784,16);
-  byte[,] hiddenLayer1Weights = RandomStartingWeights(16,16);
-  byte[,] hiddenLayer2Weights = RandomStartingWeights(16,10);
+  public double[,] inputLayerWeights;
+  public double[,] hiddenLayer1Weights;
+  public double[,] hiddenLayer2Weights;
 
   //Biases
-  byte[] inputLayerBiases = new byte[16];
-  byte[] hiddenLayer1Biases = new byte[16];
-  byte[] hiddenLayer2Biases = new byte[10];
+  public double[] inputLayerBiases;
+  public double[] hiddenLayer1Biases;
+  public double[] hiddenLayer2Biases;
 
-  //Makes a randomized byte array
-  private static byte[,] RandomStartingWeights(int width, int height)
+  //Creates neural network with saved data
+  public NeuralNetwork(double[] argumentInputLayer, double[] argumentHiddenLayer1, double[] argumentHiddenLayer2, double[] argumentOutputLayer, double[,] argumentInputLayerWeights, double[,] argumentHiddenLayer1Weights, double[,] argumentHiddenLayer2Weights, double[] argumentInputLayerBiases, double[] argumentHiddenLayer1Biases, double[] argumentHiddenLayer2Biases)
   {
-    byte[,] randomWeights = new byte[width, height];
+    inputLayer = argumentInputLayer;
+    hiddenLayer1 = argumentHiddenLayer1;
+    hiddenLayer2 = argumentHiddenLayer2;
+    outputLayer = argumentOutputLayer;
+    inputLayerWeights = argumentInputLayerWeights;
+    hiddenLayer1Weights = argumentHiddenLayer1Weights;
+    hiddenLayer2Weights = argumentHiddenLayer2Weights;
+    inputLayerBiases = argumentInputLayerBiases;
+    hiddenLayer1Biases = argumentHiddenLayer1Biases;
+    hiddenLayer2Biases = argumentHiddenLayer2Biases;
+  }
+
+  //If no information about the network is given, generate random weights and biases
+  public NeuralNetwork()
+  {
     Random random = new Random();
-    for(int y = 0; y < height; y++)
+    inputLayer = RandomDoubleArray(784, random);
+    hiddenLayer1 = RandomDoubleArray(16, random);
+    hiddenLayer2 = RandomDoubleArray(16, random);
+    outputLayer = RandomDoubleArray(10, random);
+    inputLayerWeights = Random2DDoubleArray(784, 16, random);
+    hiddenLayer1Weights = Random2DDoubleArray(16, 16, random);
+    hiddenLayer2Weights = Random2DDoubleArray(16, 10, random);
+    inputLayerBiases = RandomDoubleArray(16, random);
+    hiddenLayer1Biases = RandomDoubleArray(16, random);
+    hiddenLayer2Biases = RandomDoubleArray(10, random);
+  }
+  
+  //Makes a randomized 2D double array
+  private static double[,] Random2DDoubleArray(int width, int height, Random random)
+  {
+    double[,] random2DDoubleArray = new double[height, width];
+    for(int x = 0; x < width; x++)
     {
-      for(int x = 0; x < width; x++)
+      for(int y = 0; y < height; y++)
       {
-        randomWeights[x,y] = Convert.ToByte(random.Next(0, 255));
+        random2DDoubleArray[y,x] = random.NextDouble();
       }
     }
-    return randomWeights;
+    return random2DDoubleArray;
+  }
+
+  //Makes a randomized double array
+  private static double[] RandomDoubleArray(int length, Random random)
+  {
+    double[] randomDoubleArray = new double[length];
+    for(int i = 0; i < length; i++)
+    {
+      randomDoubleArray[i] = random.NextDouble();
+    }
+    return randomDoubleArray;
   }
 }
