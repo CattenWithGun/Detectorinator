c1520a6396fa0bfb75f6cba7b7281073a4c2f613
Catten <99224186+CattenWithGun@users.noreply.github.com>
2024-02-27 13:05:17 -0800

    Started making more notes

--- main.cs
+++ main.cs
@@ -27,7 +27,7 @@ public class NeuralNetwork
   public double[] hiddenLayer1Biases;
   public double[] hiddenLayer2Biases;
 
-  //Creates neural network with saved data
+  //Creates neural network with saved data about layers, weights, and biases
   public NeuralNetwork(double[] argumentInputLayer, double[] argumentHiddenLayer1, double[] argumentHiddenLayer2, double[] argumentOutputLayer, double[,] argumentInputLayerWeights, double[,] argumentHiddenLayer1Weights, double[,] argumentHiddenLayer2Weights, double[] argumentInputLayerBiases, double[] argumentHiddenLayer1Biases, double[] argumentHiddenLayer2Biases)
   {
     inputLayer = argumentInputLayer;
@@ -42,10 +42,11 @@ public class NeuralNetwork
     hiddenLayer2Biases = argumentHiddenLayer2Biases;
   }
 
-  //If no information about the network is given, generate random weights and biases
+  //If no information about the network is given, generate random layer values, weights, and biases
   public NeuralNetwork()
   {
     Random random = new Random();
+    //Input layer has 784 neurons because the AI will be detecting numbers 0-9 on a 28x28 image
     inputLayer = RandomDoubleArray(784, random);
     hiddenLayer1 = RandomDoubleArray(16, random);
     hiddenLayer2 = RandomDoubleArray(16, random);
@@ -58,7 +59,7 @@ public class NeuralNetwork
     hiddenLayer2Biases = RandomDoubleArray(10, random);
   }
   
-  //Makes a randomized 2D double array
+  //Makes a randomized 2D double array with width and height, used for making weights when no save data is given
   private static double[,] Random2DDoubleArray(int width, int height, Random random)
   {
     double[,] random2DDoubleArray = new double[height, width];
@@ -72,7 +73,7 @@ public class NeuralNetwork
     return random2DDoubleArray;
   }
 
-  //Makes a randomized double array
+  //Makes a randomized double array with a length, used for making layers and biases when no save data is given
   private static double[] RandomDoubleArray(int length, Random random)
   {
     double[] randomDoubleArray = new double[length];
