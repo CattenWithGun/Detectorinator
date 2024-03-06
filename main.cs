212b8a505f3e63955dc9accf7e6d7c6824ed5cb8
Catten <99224186+CattenWithGun@users.noreply.github.com>
2024-03-06 13:04:47 -0800

    Added a "Commands" class to make the Main method more readable

--- main.cs
+++ main.cs
@@ -6,11 +6,15 @@ public class Program
 {
   public static void Main()
   {
+    //Creates a list to store networks and their names
     List<NeuralNetwork> networks = new List<NeuralNetwork>();
     List<string> networkNames = new List<string>();
+
+    //This network is used as a placeholder, and gets overwritten often
     NeuralNetwork network = new NeuralNetwork("initialNetwork");
+
     Console.Clear();
-    ShowOptions();
+    Commands.ShowCommands();
     while(true)
     {
       string command = Prompts.CommandPrompt();
@@ -18,92 +22,19 @@ public class Program
       //Finds the option and does it
       if(command == "show")
       {
-        ShowNetworks(networks);
+        Commands.ShowNetworks(networks);
       }
       else if(command == "store")
       {
-        if(networks.Count == 0)
-        {
-          Console.WriteLine("There are no networks to store");
-          continue;
-        }
-        string nameToStore = Prompts.NetworkNameStorePrompt(networkNames);
-        if(nameToStore == "exit") { continue; }
-        for(int i = 0; i < networks.Count; i++)
-        {
-          if(networks[i].name == nameToStore)
-          {
-            //Serialize
-            string json = networks[i].ToString();
-            Console.WriteLine($"Saved {nameToStore} as {json}");
-            break;
-          }
-        }
+        Commands.Store(networks, networkNames);
       }
       else if(command == "make")
       {
-        string useSaveDataString = Prompts.SaveDataPrompt();
-        if(useSaveDataString == "exit") { continue; }
-        bool useSaveData = useSaveDataString == "y";
-        if(useSaveData)
-        {
-          //Get and convert save data to an object
-          Console.Write("Enter the network save data: ");
-          string neuralNetworkJson = Console.ReadLine();
-          if(neuralNetworkJson == "exit") { continue; }
-          try
-          {
-            network = JsonConvert.DeserializeObject<NeuralNetwork>(neuralNetworkJson);
-          }
-          catch
-          {
-            Console.WriteLine("Error reading save data");
-            continue;
-          }
-          
-          //If there is already a network with the name, make the user make a new one
-          if(Checks.NetworkListContainsName(networkNames, network.name))
-          {
-            Console.WriteLine("There is already a network with that name");
-            string name = Prompts.NetworkNamePrompt(networkNames);
-            if(name == "exit") { continue; }
-            network.name = name;
-          }
-
-          //Update the lists
-          networkNames.Add(network.name);
-          networks.Add(network);
-          Console.WriteLine($"{network.name} created from save data");
-        }
-        else
-        {
-          string name = Prompts.NetworkNamePrompt(networkNames);
-          if(name == "exit") { continue; }
-          network = new NeuralNetwork(name);
-          networks.Add(network);
-          networkNames.Add(name);
-          Console.WriteLine($"\nCreated {network.name}");
-        }
+        Commands.Make(networks, networkNames);
       }
       else if(command == "delete")
       {
-        if(networks.Count == 0)
-        {
-          Console.WriteLine("There are no networks to delete");
-          continue;
-        }
-        
-        string nameToDelete = Prompts.NetworkNameDeletePrompt(networkNames);
-        if(nameToDelete == "exit") { continue; }
-        
-        for(int i = 0; i < networks.Count; i++)
-        {
-          if(networks[i].name == nameToDelete)
-          {
-            networks.RemoveAt(i);
-          }
-        }
-        Console.WriteLine($"\nDeleted {nameToDelete}");
+        Commands.Delete(networks, networkNames);
       }
       else if(command == "clear")
       {
@@ -111,12 +42,125 @@ public class Program
       }
       else if(command == "help")
       {
-        ShowOptions();
+        Commands.ShowCommands();
       }
     }
   }
+}
 
-  private static void ShowNetworks(List<NeuralNetwork> neuralNetworks)
+public static class Commands
+{
+  public static void Delete(List<NeuralNetwork> networks, List<string> networkNames)
+  {
+    if(networks.Count == 0)
+    {
+      Console.WriteLine("There are no networks to delete");
+      return;
+    }
+
+    //Get the name of the network to be deleted
+    string nameToDelete = Prompts.NetworkNameDeletePrompt(networkNames);
+    if(nameToDelete == "exit") { return; }
+
+    //Find the network in the list and delete it
+    for(int i = 0; i < networks.Count; i++)
+    {
+      if(networks[i].name == nameToDelete)
+      {
+        networks.RemoveAt(i);
+        networkNames.RemoveAt(i);
+      }
+    }
+    Console.WriteLine($"\nDeleted {nameToDelete}");
+  }
+  
+  public static void Make(List<NeuralNetwork> networks, List<string> networkNames)
+  {
+    if(networks.Count == 20)
+    {
+      Console.WriteLine("You know what? No. You don't get anymore than 20 networks because no sane human being would ever need that many.");
+      return;
+    }
+    string useSaveDataString = Prompts.SaveDataPrompt();
+    if(useSaveDataString == "exit") { return; }
+    bool useSaveData = useSaveDataString == "y";
+
+    if(useSaveData)
+    {
+      //Get save data from user
+      Console.Write("Enter the network save data: ");
+      string networkJson = Console.ReadLine();
+      if(networkJson == "exit") { return; }
+
+      //Convert to a NeuralNetwork
+      NeuralNetwork network = JsonToNetwork(networkJson);
+      if(network == null) { return; }
+
+      //If there is already a network with the name, make the user make a new one
+      if(Checks.NetworkListContainsName(networkNames, network.name))
+      {
+        Console.WriteLine("There is already a network with that name");
+        string name = Prompts.NetworkNamePrompt(networkNames);
+        if(name == "exit") { return; }
+        network.name = name;
+      }
+
+      //Add the network to list of networks
+      networkNames.Add(network.name);
+      networks.Add(network);
+      Console.WriteLine($"{network.name} created from save data");
+    }
+    else
+    {
+      //Gets name
+      string name = Prompts.NetworkNamePrompt(networkNames);
+      if(name == "exit") { return; }
+
+      //Creates a randomized network with the name
+      NeuralNetwork network = new NeuralNetwork(name);
+
+      //Add the network to list of networks
+      networks.Add(network);
+      networkNames.Add(name);
+      Console.WriteLine($"\nCreated {network.name}");
+    }
+  }  
+  
+  public static void Store(List<NeuralNetwork> networks, List<string> networkNames)
+  {
+    if(networks.Count == 0)
+    {
+      Console.WriteLine("There are no networks to store");
+      return;
+    }
+    string nameToStore = Prompts.NetworkNameStorePrompt(networkNames);
+    if(nameToStore == "exit") { return; }
+    for(int i = 0; i < networks.Count; i++)
+    {
+      if(networks[i].name == nameToStore)
+      {
+        //Serialize
+        string json = networks[i].ToString();
+        Console.WriteLine($"Saved {nameToStore} as {json}");
+        return;
+      }
+    }
+  }
+  
+  public static void ShowCommands()
+  {
+    Console.WriteLine("Type commands to do stuff:");
+    Console.WriteLine("Show list of neural networks:     show");
+    Console.WriteLine("Train neural network:             train");
+    Console.WriteLine("Store neural network:             store");
+    Console.WriteLine("Make new neural network:          make");
+    Console.WriteLine("Delete neural network:            delete");
+    Console.WriteLine("Exits out of a prompt:            exit");
+    Console.WriteLine("Clear the screen:                 clear");
+    Console.WriteLine("Show this dialogue again:         help");
+  }
+
+  public static void ShowNetworks(List<NeuralNetwork> neuralNetworks)
   {
     Console.WriteLine("\nSaved Neural Networks:\n");
     if(neuralNetworks.Count == 0)
@@ -130,17 +174,17 @@ public class Program
     }
   }
 
-  private static void ShowOptions()
+  private static NeuralNetwork JsonToNetwork(string networkJson)
   {
-    Console.WriteLine("Type commands to do stuff:");
-    Console.WriteLine("Show list of neural networks:     show");
-    Console.WriteLine("Train neural network:             train");
-    Console.WriteLine("Store neural network:             store");
-    Console.WriteLine("Make new neural network:          make");
-    Console.WriteLine("Delete neural network:            delete");
-    Console.WriteLine("Exits out of a prompt:            exit");
-    Console.WriteLine("Clear the screen:                 clear");
-    Console.WriteLine("Show this dialogue again:         help");
+    try
+    {
+      return JsonConvert.DeserializeObject<NeuralNetwork>(networkJson);
+    }
+    catch
+    {
+      Console.WriteLine("Error reading save data");
+      return null;
+    }
   }
 }
 
