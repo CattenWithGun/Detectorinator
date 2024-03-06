a522bbdfc5778b7928c082da9b775cf45613a991
Catten <99224186+CattenWithGun@users.noreply.github.com>
2024-03-06 09:36:03 -0800

    Added ability save and use save data to recreate an old network

--- main.cs
+++ main.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 public class Program
 {
@@ -12,57 +13,64 @@ public class Program
     ShowOptions();
     while(true)
     {
-      //Gets an option
-      Console.Write("\nEnter: ");
-      string option = Console.ReadLine();
-      while(!IsOption(option))
-      {
-        Console.Write("\nNot a command, enter again: ");
-        option = Console.ReadLine();
-      }
+      string command = Prompts.CommandPrompt();
 
       //Finds the option and does it
-      if(option == "show")
+      if(command == "show")
       {
         ShowNetworks(networks);
       }
-      else if(option == "store")
+      else if(command == "store")
       {
         if(networks.Count == 0)
         {
           Console.WriteLine("There are no networks to store");
           continue;
         }
-        string nameToStore = NetworkNameStorePrompt(networkNames);
+        string nameToStore = Prompts.NetworkNameStorePrompt(networkNames);
         for(int i = 0; i < networks.Count; i++)
         {
           if(networks[i].name == nameToStore)
           {
-            network = networks[i];
+            //Serialize
+            string json = networks[i].ToString();
+
+            Console.WriteLine($"Saved {nameToStore} as {json}");
+            break;
           }
         }
-        
       }
-      else if(option == "make")
+      else if(command == "make")
       {
-        string name = NetworkNamePrompt(networkNames);
-        bool useSaveData = SaveDataPrompt();
+        bool useSaveData = Prompts.SaveDataPrompt();
         if(useSaveData)
         {
-          Console.WriteLine("unfinished");
+          Console.Write("Enter the network save data: ");
+          string neuralNetworkJson = Console.ReadLine();
+
+          //Deserialize
+          network = JsonConvert.DeserializeObject<NeuralNetwork>(neuralNetworkJson);
+
+          if(Checks.NetworkListContainsName(networkNames, network.name))
+          {
+            Console.WriteLine("The save data already has a network with that name");
+            network.name = Prompts.NetworkNamePrompt(networkNames);
+          }
+          networks.Add(network);
+          Console.WriteLine($"{network.name} created from save data");
         }
         else
         {
-          //might need to make sure this doesn't do a memory leak
+          string name = Prompts.NetworkNamePrompt(networkNames);
           network = new NeuralNetwork(name);
           networks.Add(network);
           networkNames.Add(name);
           Console.WriteLine($"\nCreated {network.name}");
         }
       }
-      else if(option == "delete")
+      else if(command == "delete")
       {
-        string nameToDelete = NetworkNameDeletePrompt(networkNames);
+        string nameToDelete = Prompts.NetworkNameDeletePrompt(networkNames);
         for(int i = 0; i < networks.Count; i++)
         {
           if(networks[i].name == nameToDelete)
@@ -72,117 +80,15 @@ public class Program
         }
         Console.WriteLine($"\nDeleted {nameToDelete}");
       }
-      else if(option == "clear")
+      else if(command == "clear")
       {
         Console.Clear();
       }
-      else if(option == "help")
+      else if(command == "help")
       {
         ShowOptions();
       }
     }
-  }
-
-  private static string NetworkToString(NeuralNetwork network)
-  {
-    string output = "";
-    string name = network.name;
-    string inputLayer = DoubleArrayToString(network.inputLayer);
-    string hiddenLayer1 = DoubleArrayToString(network.hiddenLayer1);
-    string hiddenLayer2 = DoubleArrayToString(network.hiddenLayer2);
-    string outputLayer = DoubleArrayToString(network.outputLayer);
-    /*
-
-    //Weights
-    public double[,] inputLayerWeights;
-    public double[,] hiddenLayer1Weights;
-    public double[,] hiddenLayer2Weights;
-
-    //Biases
-    public double[] inputLayerBiases;
-    public double[] hiddenLayer1Biases;
-    public double[] hiddenLayer2Biases;
-    */
-    
-    return output;
-  }
-
-  private static string Double2DArrayToString(double[,] double2DArray)
-  {
-    string array = "[[";
-    for(int row = 0; row < double2DArray.GetLength(0); row++)
-    {
-      for(int column = 0; column < double2DArray.GetLength(1); column++)
-      {
-        
-      }
-      array += "],";
-    }
-    array += "]";
-    return array;
-  }
-
-  private static string NetworkNameStorePrompt(List<string> networkNames)
-  {
-    Console.Write("Enter a network to store: ");
-    string nameToStore = Console.ReadLine();
-    while(!NetworkListContainsName(networkNames, nameToStore))
-    {
-      Console.WriteLine("Network wasn't in the saved networks list");
-      Console.Write("Enter again: ");
-      nameToStore = Console.ReadLine();
-    }
-    return nameToStore;
-  }
-
-  private static string NetworkNameDeletePrompt(List<string> networkNames)
-  {
-    Console.Write("Enter a network you want to delete: ");
-    string nameToDelete = Console.ReadLine();
-    while(!NetworkListContainsName(networkNames, nameToDelete))
-    {
-      Console.WriteLine("Name wasn't in the saved networks list");
-      Console.Write("Enter again: ");
-      nameToDelete = Console.ReadLine();
-    }
-    return nameToDelete;
-  }
-
-  private static string NetworkNamePrompt(List<string> networkNames)
-  {
-    Console.Write("Enter a name for the network: ");
-    string name = Console.ReadLine();
-    while(name.Length > 20 || name.Length <= 0 || NetworkListContainsName(networkNames, name))
-    {
-      Console.WriteLine("Name length needs to be 1-20 characters and not already used");
-      Console.Write("Enter again: ");
-      name = Console.ReadLine();
-    }
-    return name;
-  }
-
-  private static bool NetworkListContainsName(List<string> networkNames, string name)
-  {
-    for(int i = 0; i < networkNames.Count; i++)
-    {
-      if(networkNames[i] == name)
-      {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  private static bool SaveDataPrompt()
-  {
-    string useSaveDataString;
-    do
-    {
-      Console.Write("Do you want to use save data? (y/n): ");
-      useSaveDataString = Console.ReadLine();
-    }
-    while(useSaveDataString != "y" && useSaveDataString != "n");
-    return useSaveDataString == "y";
   }
 
   private static void ShowNetworks(List<NeuralNetwork> neuralNetworks)
@@ -210,8 +116,23 @@ public class Program
     Console.WriteLine("Clear the screen:                 clear");
     Console.WriteLine("Show this dialogue again:         help");
   }
+}
 
-  private static bool IsOption(string option)
+public static class Checks
+{
+  public static bool NetworkListContainsName(List<string> networkNames, string name)
+  {
+    for(int i = 0; i < networkNames.Count; i++)
+    {
+      if(networkNames[i] == name)
+      {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public static bool IsOption(string option)
   {
     //Goes through the list of commands, and if the option is in there, return that it is a command
     string[] commands = { "show", "train", "store", "make", "delete", "clear", "help" };
@@ -226,36 +147,69 @@ public class Program
   }
 }
 
-public static class DataToString
+public static class Prompts
 {
-  public static string DoubleArrayToString(double[] doubleArray)
+  public static string CommandPrompt()
   {
-    string array = "[";
-    for(int i = 0; i < doubleArray.Length; i++)
+    Console.Write("\nEnter: ");
+    string option = Console.ReadLine();
+    while(!Checks.IsOption(option))
     {
-      array += Convert.ToString(doubleArray[i]) + ",";
+      Console.Write("\nNot a command, enter again: ");
+      option = Console.ReadLine();
     }
-    array += "]";
-    return array;
+    return option;
   }
 
-  public static double[] StringToDoubleArray(string stringArray)
+  public static string NetworkNameStorePrompt(List<string> networkNames)
   {
-    string currentString = "";
-    List<double> doubles = new List<double>();
-    for(int i = 1; i < stringArray.Length; i++)
+    Console.Write("Enter a network to store: ");
+    string nameToStore = Console.ReadLine();
+    while(!Checks.NetworkListContainsName(networkNames, nameToStore))
     {
-      if(stringArray[i] == ',')
-      {
-        doubles.Add(Convert.ToDouble(currentString));
-        currentString = "";
-      }
-      else if(stringArray[i] != ']')
-      {
-        currentString += stringArray[i];
-      }
+      Console.WriteLine("Network wasn't in the saved networks list");
+      Console.Write("Enter again: ");
+      nameToStore = Console.ReadLine();
     }
-    return doubles.ToArray();
+    return nameToStore;
+  }
+
+  public static string NetworkNameDeletePrompt(List<string> networkNames)
+  {
+    Console.Write("Enter a network you want to delete: ");
+    string nameToDelete = Console.ReadLine();
+    while(!Checks.NetworkListContainsName(networkNames, nameToDelete))
+    {
+      Console.WriteLine("Name wasn't in the saved networks list");
+      Console.Write("Enter again: ");
+      nameToDelete = Console.ReadLine();
+    }
+    return nameToDelete;
+  }
+
+  public static string NetworkNamePrompt(List<string> networkNames)
+  {
+    Console.Write("Enter a name for the network: ");
+    string name = Console.ReadLine();
+    while(name.Length > 20 || name.Length <= 0 || Checks.NetworkListContainsName(networkNames, name))
+    {
+      Console.WriteLine("Name length needs to be 1-20 characters and not already used");
+      Console.Write("Enter again: ");
+      name = Console.ReadLine();
+    }
+    return name;
+  }
+
+  public static bool SaveDataPrompt()
+  {
+    string useSaveDataString;
+    do
+    {
+      Console.Write("Do you want to use save data? (y/n): ");
+      useSaveDataString = Console.ReadLine();
+    }
+    while(useSaveDataString != "y" && useSaveDataString != "n");
+    return useSaveDataString == "y";
   }
 }
 
@@ -280,7 +234,8 @@ public class NeuralNetwork
   public double[] hiddenLayer2Biases;
 
   //Creates neural network with saved data
-  public NeuralNetwork(double[] argumentInputLayer, double[] argumentHiddenLayer1, double[] argumentHiddenLayer2, double[] argumentOutputLayer, double[,] argumentInputLayerWeights, double[,] argumentHiddenLayer1Weights, double[,] argumentHiddenLayer2Weights, double[] argumentInputLayerBiases, double[] argumentHiddenLayer1Biases, double[] argumentHiddenLayer2Biases, string constructName)
+  [JsonConstructor]
+  public NeuralNetwork(double[] argumentInputLayer, double[] argumentHiddenLayer1, double[] argumentHiddenLayer2, double[] argumentOutputLayer, double[,] argumentInputLayerWeights, double[,] argumentHiddenLayer1Weights, double[,] argumentHiddenLayer2Weights, double[] argumentInputLayerBiases, double[] argumentHiddenLayer1Biases, double[] argumentHiddenLayer2Biases, string argumentName)
   {
     inputLayer = argumentInputLayer;
     hiddenLayer1 = argumentHiddenLayer1;
@@ -292,11 +247,11 @@ public class NeuralNetwork
     inputLayerBiases = argumentInputLayerBiases;
     hiddenLayer1Biases = argumentHiddenLayer1Biases;
     hiddenLayer2Biases = argumentHiddenLayer2Biases;
-    name = constructName;
+    name = argumentName;
   }
 
   //If no neuron information about the network is given, generate random weights and biases
-  public NeuralNetwork(string constructName)
+  public NeuralNetwork(string argumentName)
   {
     Random random = new Random();
     inputLayer = RandomDoubleArray(784, random);
@@ -309,7 +264,12 @@ public class NeuralNetwork
     inputLayerBiases = RandomDoubleArray(16, random);
     hiddenLayer1Biases = RandomDoubleArray(16, random);
     hiddenLayer2Biases = RandomDoubleArray(10, random);
-    name = constructName;
+    name = argumentName;
+  }
+
+  public override string ToString()
+  {
+    return JsonConvert.SerializeObject(this);
   }
 
   //Makes a randomized 2D double array
