3065d9184dafb45c729a1f04a41bf973c3525fd8
Catten <99224186+CattenWithGun@users.noreply.github.com>
2024-03-07 11:35:34 -0800

    Added a class file for ErrorChecks class, for checking for errors on user input

---
+++ Checks.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+
+namespace ErrorChecks
+{
+  public static class Checks
+  {
+    public static bool NetworkListContainsName(List<string> networkNames, string name)
+    {
+      for(int i = 0; i < networkNames.Count; i++)
+      {
+        if(networkNames[i] == name)
+        {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    public static bool IsOption(string option)
+    {
+      //Goes through the list of commands, and if the option is in there, return that it is a command
+      string[] commands = { "show", "train", "store", "make", "delete", "clear", "help" };
+      for(int i = 0; i < commands.Length; i++)
+      {
+        if(commands[i] == option)
+        {
+          return true;
+        }
+      }
+      return false;
+    }
+  }
+}
