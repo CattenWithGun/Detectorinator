dbaea29d7caa2096192da744fc028c9809f09eaa
Catten <99224186+CattenWithGun@users.noreply.github.com>
2024-03-07 12:21:34 -0800

    Added a placeholder method to train the network

---
+++ NetworkTraining.cs
@@ -0,0 +1,15 @@
+using NeuralNetworking;
+using System;
+using System.Collections.Generic;
+
+namespace NetworkTraining
+{
+  public static class Training
+  {
+    public static NeuralNetwork TrainNetwork(NeuralNetwork network)
+    {
+      Console.WriteLine("Training network...");
+      return network;
+    }
+  }
+}
