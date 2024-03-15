90f2803cb3a72ff60faccb2b13ff6a58f5bdbcb8
Catten <99224186+CattenWithGun@users.noreply.github.com>
2024-03-14 17:15:49 -0700

    Removed the MNIST stuff from this class

--- NetworkTraining.cs
+++ NetworkTraining.cs
@@ -1,6 +1,6 @@
+using MNIST;
 using NeuralNetworking;
 using System;
-using System.IO;
 
 namespace NetworkTraining
 {
@@ -9,183 +9,22 @@ namespace NetworkTraining
     public static NeuralNetwork TrainNetwork(NeuralNetwork network)
     {
       Console.WriteLine("Training network...");
-      byte[] labels = GetLabels("/home/runner/NeuralNetwork/MNIST_Test_Database/labels");
-      byte[,,] images = GetImages("/home/runner/NeuralNetwork/MNIST_Test_Database/images");
-      images = SplitFlipAll(images);
+      byte[] labels = MNISTFileHandler.GetLabels("/home/runner/NeuralNetwork/MNIST_Test_Database/labels");
+      byte[,,] images = MNISTFileHandler.GetImages("/home/runner/NeuralNetwork/MNIST_Test_Database/images");
       Random random = new Random();
-      WriteImage(random.Next(0,9999), images, labels);
+      while(true)
+      {
+        MNISTFileHandler.WriteImage(random.Next(0, 9999), images, labels);
+        Console.ReadLine();
+      }
       return network;
     }
 
-    /*
-    Array.Copy has parameters like this:
-    Source array, Where to start at source array, New array, Where to start at new array, How many elements to copy
-    It is used to get a small section of the files to read how many elements in the file there is, but it is unreadable
-    */
-    
-    /*
-    Image File Format:
-    [offset] [type]          [value]          [description]
-    0000     32 bit integer  0x00000803(2051) magic number (shows what type of file it is)
-    0004     32 bit integer  10000            number of images
-    0008     32 bit integer  28               number of rows (height of each image)
-    0012     32 bit integer  28               number of columns (width of each image)
-    0016     unsigned byte   ??               pixel (0 to 255, 0 meaning white and 255 meaning black)
-    0017     unsigned byte   ??               pixel
-    0018     unsigned byte   ??               pixel
-    ........
-    xxxx     unsigned byte   ??               pixel
-    */
-    
-    private static byte[,,] GetImages(string pathToImageFile)
+    //Sigmoid function for squishing any value between 0 and 1
+    private static double Sigmoid(double value)
     {
-      //Gets data in the image file
-      byte[] imageFile = File.ReadAllBytes(pathToImageFile);
-
-      //Finds the number of images
-      byte[] numberOfImagesArray = new byte[4];
-      Array.Copy(imageFile, 4, numberOfImagesArray, 0, 4);
-      int numberOfImages = ByteArrayToInt(numberOfImagesArray);
-
-      //Finds the width of the images
-      byte[] widthOfImagesArray = new byte[4];
-      Array.Copy(imageFile, 12, widthOfImagesArray, 0, 4);
-      int width = ByteArrayToInt(widthOfImagesArray);
-
-      //Finds the height of the images
-      byte[] heightOfImagesArray = new byte[4];
-      Array.Copy(imageFile, 8, heightOfImagesArray, 0, 4);
-      int height = ByteArrayToInt(heightOfImagesArray);
-
-      //Puts all the images into a 3D array, like a picture book
-      byte[,,] images = new byte[numberOfImages, height, width];
-      for(int imagesIndex = 0; imagesIndex < numberOfImages; imagesIndex++)
-      {
-        for(int heightIndex = 0; heightIndex < height; heightIndex++)
-        {
-          for(int widthIndex = 0; widthIndex < width; widthIndex++)
-          {
-            images[imagesIndex, heightIndex, widthIndex] = imageFile[784*imagesIndex + 28*heightIndex + widthIndex];
-          }
-        }
-      }
-      return images;
-    }
-
-    private static void WriteImage(int index, byte[,,] images, byte[] labels)
-    {
-      string[] rows = new string[28];
-      string placeholder = "";
-      for(int heightIndex = 0; heightIndex < 28; heightIndex++)
-      {
-        for(int widthIndex = 0; widthIndex < 28; widthIndex++)
-        {
-          placeholder += ConvertToPixel(Convert.ToString(images[index, heightIndex, widthIndex]));
-        }
-        rows[heightIndex] = placeholder;
-        placeholder = "";
-      }
-      for(int i = 0; i < 28; i++)
-      {
-        Console.WriteLine(rows[i]);
-      }
-      Console.WriteLine(labels[index]);
-    }
-
-    private static byte[,,] SplitFlipAll(byte[,,] images)
-    {
-      for(int imageIndex = 0; imageIndex < 10000; imageIndex++)
-      {
-        for(int heightIndex = 0; heightIndex < 28; heightIndex++)
-        {
-          byte[] row = new byte[28];
-          for(int widthIndex = 0; widthIndex < 28; widthIndex++)
-          {
-            row[widthIndex] = images[imageIndex, heightIndex, widthIndex];
-          }
-          row = SplitFlip(row);
-          for(int widthIndex = 0; widthIndex < 28; widthIndex++)
-          {
-            images[imageIndex, heightIndex, widthIndex] = row[widthIndex];
-          }
-        }
-      }
-      return images;
-    }
-
-    private static byte[] SplitFlip(byte[] row)
-    {
-      byte[] leftSide = new byte[14];
-      Array.Copy(row, 0, leftSide, 0, 14);
-      byte[] rightSide = new byte[14];
-      Array.Copy(row, 14, rightSide, 0, 14);
-      byte[] finalArray = new byte[28];
-      Array.Copy(rightSide, 0, finalArray, 0, 14);
-      Array.Copy(leftSide, 0, finalArray, 14, 14);
-      return finalArray;
-    }
-    
-    /*
-    Label File Format:
-    [offset] [type]          [value]          [description]
-    0000     32 bit integer  0x00000801(2049) magic number (shows what type of file it is)
-    0004     32 bit integer  10000            number of labels
-    0008     unsigned byte   ??               label (the number that the image represents, 0 to 9)
-    0009     unsigned byte   ??               label
-    0010     unsigned byte   ??               label
-    ........
-    xxxx     unsigned byte   ??               label
-    */
-    
-    private static byte[] GetLabels(string pathToLabelFile)
-    {
-      //Gets data in the label file
-      byte[] labelFile = File.ReadAllBytes(pathToLabelFile);
-
-      //Finds the number of labels
-      byte[] numberOfLabelsArray = new byte[4];
-      Array.Copy(labelFile, 4, numberOfLabelsArray, 0, 4);
-      int numberOfLabels = ByteArrayToInt(numberOfLabelsArray);
-
-      //Puts the labels into an array
-      byte[] labels = new byte[numberOfLabels];
-      for(int i = 8; i < numberOfLabels + 8; i++)
-      {
-        labels[i - 8] = labelFile[i];
-      }
-      return labels;
-    }
-
-    private static int ByteArrayToInt(byte[] bytes)
-    {
-      //If the computer uses Little Endian, change it to Big Endian because the image and label files are in Big Endian
-      if(BitConverter.IsLittleEndian)
-      {
-        Array.Reverse(bytes);
-      }
-      return BitConverter.ToInt32(bytes, 0);
-    }
-
-    private static string ConvertToPixel(string number)
-    {
-      byte shade = Convert.ToByte(number);
-      if(shade < 51)
-      {
-        return "█";
-      }
-      else if(shade >= 51 && shade < 102)
-      {
-        return "▓";
-      }
-      else if(shade >= 102 && shade < 153)
-      {
-        return "▒";
-      }
-      else if(shade >= 153 && shade < 204)
-      {
-        return "░";
-      }
-      return " ";
+      double k = Math.Exp(value);
+      return k / (1 + k);
     }
   }
 }
