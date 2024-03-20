d73e389c8cd9b7f43cb35e5d920f9694ac3d38a4
Catten <99224186+CattenWithGun@users.noreply.github.com>
2024-03-20 10:15:28 -0700

    Update NeuralNetwork.cs

--- NeuralNetwork.cs
+++ NeuralNetwork.cs
@@ -1,5 +1,6 @@
 using Newtonsoft.Json;
 using System;
+using System.Linq;
 
 namespace NeuralNetworking
 {
@@ -48,9 +49,11 @@ namespace NeuralNetworking
       hiddenLayer1 = RandomDoubleArray(16, random);
       hiddenLayer2 = RandomDoubleArray(16, random);
       outputLayer = RandomDoubleArray(10, random);
+      
       inputLayerWeights = Random2DDoubleArray(784, 16, random);
       hiddenLayer1Weights = Random2DDoubleArray(16, 16, random);
       hiddenLayer2Weights = Random2DDoubleArray(16, 10, random);
+      
       inputLayerBiases = RandomDoubleArray(16, random);
       hiddenLayer1Biases = RandomDoubleArray(16, random);
       hiddenLayer2Biases = RandomDoubleArray(10, random);
@@ -85,6 +88,73 @@ namespace NeuralNetworking
         randomDoubleArray[i] = random.NextDouble();
       }
       return randomDoubleArray;
+    }
+
+    //Sigmoid function for squishing any value between 0 and 1 using doubles
+    private static double Sigmoid(double value)
+    {
+      double k = Math.Exp(value);
+      return k / (1 + k);
+    }
+
+    //Sigmoid function for squishing any value between 0 and 1 using bytes
+    private static double Sigmoid(byte valueByte)
+    {
+      double value = Convert.ToDouble(valueByte);
+      double k = Math.Exp(value);
+      return k / (1 + k);
+    }
+
+    public byte FeedForward(byte[] inputLayerBytes, NeuralNetwork network)
+    {
+      //Turns the inputLayerBytes into doubles and puts them in the network's inputLayer
+      for(int i = 0; i < network.inputLayer.Length; i++)
+      {
+        //network.inputLayer[i] = Sigmoid(inputLayerBytes[i]);
+        network.inputLayer[i] = Convert.ToDouble(inputLayerBytes[i]) / 255;
+      }
+      
+      //Feed forward to hiddenLayer1
+      for(int neuronIndex = 0; neuronIndex < network.hiddenLayer1.Length; neuronIndex++)
+      {
+        double currentStep = 0;
+        for(int weightIndex = 0; weightIndex < network.inputLayer.Length; weightIndex++)
+        {
+          currentStep += (network.inputLayerWeights[neuronIndex, weightIndex] * inputLayer[weightIndex]);
+        }
+        currentStep += network.inputLayerBiases[neuronIndex];
+        currentStep = Sigmoid(currentStep);
+        network.hiddenLayer1[neuronIndex] = currentStep;
+      }
+
+      //Feed forward to hiddenLayer2
+      for(int neuronIndex = 0; neuronIndex < network.hiddenLayer2.Length; neuronIndex++)
+      {
+        double currentStep = 0;
+        for(int weightIndex = 0; weightIndex < network.hiddenLayer1.Length; weightIndex++)
+        {
+          currentStep += (network.hiddenLayer1Weights[neuronIndex, weightIndex] * hiddenLayer1[weightIndex]);
+        }
+        currentStep += network.hiddenLayer1Biases[neuronIndex];
+        currentStep = Sigmoid(currentStep);
+        network.hiddenLayer2[neuronIndex] = currentStep;
+      }
+
+      //Feed forward to outputLayer
+      for(int neuronIndex = 0; neuronIndex < network.outputLayer.Length; neuronIndex++)
+      {
+        double currentStep = 0;
+        for(int weightIndex = 0; weightIndex < network.hiddenLayer2.Length; weightIndex++)
+        {
+          currentStep += (network.hiddenLayer2Weights[neuronIndex, weightIndex] * hiddenLayer2[weightIndex]);
+        }
+        currentStep += network.hiddenLayer2Biases[neuronIndex];
+        currentStep = Sigmoid(currentStep);
+        network.outputLayer[neuronIndex] = currentStep;
+      }
+      
+      //Finds the highest value in the output layer to determine the most probable number the image represents
+      return Convert.ToByte(network.outputLayer.ToList().IndexOf(network.outputLayer.Max()));
     }
   }
 }
